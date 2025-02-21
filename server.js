fcdb7d4c2c9caca8c485ec8b4920fa5b7d095166
gavrilasa <gavrilasamanaa@gmail.com>
2025-02-21 13:31:16 +0700

    Learn setHeader

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require("http");
 
 const requestListener = (request, response) => {
-  response.setHeader("Content-Type", "text/html");
+  response.setHeader('Content-Type', 'application/json');
+  response.setHeader('Powered-By', 'Node.js');
   response.statusCode = 200;
 
   const { method, url } = request;
